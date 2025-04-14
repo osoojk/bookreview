6b1845e086843554bf835a6a3fd9e83d90f6536c
AJ <ashlynjoshy@gamil.com>
2025-04-14 17:55:25 -0400

    added search file

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import BookList from "./pages/BookList";
+import Search from "./pages/Search";
 import Navbar from "./components/Navbar";
+
 // import BookDetail from "./pages/BookDetail";
 // import Login from "./pages/Login";
 // import Signup from "./pages/Signup";
@@ -15,8 +17,9 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/books" element={<BookList />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/books" element={<BookList />} />
+          <Route path="/search" element={<Search />} />
         {/* <Route path="/books/:id" element={<BookDetail />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} /> */}
